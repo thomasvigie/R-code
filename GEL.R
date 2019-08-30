bf20574dec260476d290bd0eb8cfd12720322c99
thomasvigie <40642803+thomasvigie@users.noreply.github.com>
2019-08-30 09:33:42 -0700

    Add files via upload

---
+++ GEL.R
@@ -0,0 +1,337 @@
+# This file contains functions that estimate parameters in moment restrictions through Generalized Empirical Likelihood methods. It include:
+#   - The Empirical Likelihood (EL) estimator
+#   - The Exponential Tilting (ET) estimaor
+#   - The Hellinger Distance (HD) estimator from Kitamura, Otsu and Evdokimov (2013)
+#   - The Exponential Tilting Empirical (ETEL) Likelihood estimator from Schennach (2007)
+#   - The Exponential Tilting estimator Hellinger (ETHD) Distance estimator from Antoine and Dovonon (2018)
+#   - The continuously updated estimator (CU)
+# The function uses the global optimization algorithm "DEoptim" from Ardia et al. (2012)
+
+# Details about the function --------------------------
+
+# The functions are:
+#   - smoothG: in time series contexts, the moment conditions can smoothed out to account for serial dependence. Same function as in the "gmm" package.
+#   - inner: function that returns the value of the inner function, a function of the Lagrange multipliers lambdas and theta, the parameter vector of interest.
+#   - innergradient: function that returns the gradient of the inner function for each type of likelihood. Used in "getlambdas".
+#   - outer: returns the outer function for any value of theta, i.e. the "inner" function after it is minimized w.r.t. lambdas.
+#   - getlambdas: for each type of likelihood function, computes the optimal Lagrange multipliers lambdas by solving the "inner" problem
+#   - impliedprobs: computes the probabilities on each observation that are implied by the Lagrange multipliers found after solving the "inner" problem.
+#   - likelihood: computes the likelihood function using the implied probabilities (via the function "impliedprobs") computed for each type of empirical likelihood estimator. 
+#                 The implied probabilities are a function of the Lagrange multipliers, computed via "getlambdas".
+#   - GEL: the final function that computes the estimates by minimizing the "outer" function. It uses the "DEoptim" package. It returns the estimates of theta, the value of the objective function,
+#          the corresponding Lagrange multipliers and the implied probabilities.
+                 
+
+list.of.packages <- c('DEoptim','sandwich','gmm')
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+library('DEoptim')   # algorithm used for the GMM and GEL estimation
+library('sandwich')  # used to smooth the moment function in the GEl case (See Kitamura and Sutzer (1997), Smith (1997), anatolyev(2005) )
+library('gmm')
+
+smoothG <- function (dat, bw = bwNeweyWest, prewhite = 0, ar.method = "ols", 
+                     weights = weightsAndrews, kernel = c("Bartlett", "Parzen", 
+                                                          "Truncated", "Tukey-Hanning"), approx = c("AR(1)", "AR(1)"), 
+                     tol = 1e-07) 
+{
+  kernel <- match.arg(kernel)
+  approx <- match.arg(approx)
+  n <- nrow(dat)
+  if (is.function(weights)) {
+    class(dat) <- "gmmFct"
+    w <- weights(dat, bw = bw, kernel = kernel, prewhite = prewhite, 
+                 ar.method = ar.method, tol = tol, verbose = FALSE, 
+                 approx = approx)
+  }
+  else {
+    w <- weights
+  }
+  if (is.numeric(w)) {
+    rt <- length(w)
+    if (rt >= 2) {
+      w <- c(w[rt:2], w)
+      w <- w/sum(w)
+      w <- kernel(w[rt:length(w)])
+    }
+    else {
+      w <- kernel(1)
+    }
+  }
+  else {
+    if (class(w) != "tskernel") 
+      stop("Provided weights must be a numeric vector or an object of class 'tskernel'")
+  }
+  if (length(w$coef) > 1) 
+    dat <- kernapply(dat, w)
+  sx <- list(smoothx = dat, kern_weights = w, bw = bw)
+  return(sx)
+}
+
+inner <- function(momentfun , lambda , theta , dat,type = c("ET","EL","HD","CU"),smooth = c(TRUE,FALSE) , delta)
+  
+{
+  if(missing(smooth))
+  { smooth = FALSE }
+  
+  m <- momentfun(theta,x = dat )
+  if (smooth == TRUE )
+  {
+    m <- smoothG( m , bw = bwAndrews, prewhite = 0, ar.method = "ols", 
+                  weights = weightsAndrews, kernel = c( "Truncated"),  
+                  tol = 1e-07)$smoothx
+  }
+  
+  options(warn=-1)
+    n <- nrow(m)
+    lambda <- matrix(lambda , ncol = 1)
+
+    nu <- as.vector(m%*%lambda )
+    if (type == "ET")
+    {   
+      h <- - exp(nu)
+    } 
+    if (type == "EL")
+    {
+      y <- 1 - nu
+      h <- as.matrix(ifelse(y > delta , log(y) , log(delta)-1.5+2*y/delta-y^2/(2*delta^2) ))
+    } 
+    if (type == "HD")
+    {
+      h <- 2/(1-nu/2)
+    } 
+    if (type == "CU")
+    {
+      h <- -((1 + nu)^2)/2
+    }
+  return(-mean(h))
+  
+}
+
+
+innergradient <- function(theta , lambda , momentfun ,dat, type = c("ET","EL","HD","CU"),smooth = c(TRUE,FALSE),delta )
+{
+  
+  if(missing(smooth))
+  { smooth = FALSE }
+  
+  m <- momentfun(theta,x = dat )
+  if (smooth == TRUE )
+  {
+    m <- smoothG( m , bw = bwAndrews, prewhite = 0, ar.method = "ols",
+                  weights = weightsAndrews, kernel = c( "Truncated"),
+                  tol = 1e-07)$smoothx
+  }
+  n <- nrow(m)
+  lambda <- matrix(lambda , ncol = 1)
+  nmoments <- ncol(m)
+  nu <-  as.vector(m%*%lambda)
+    
+    if (type == "EL")
+    {
+      y <- matrix( rep( 1 - nu , each = nmoments  ) , ncol = nmoments , byrow = TRUE ) # We repeat the column to match the dimensions of m 
+      h <- as.matrix(ifelse(y>delta , -m/y , -(2/delta)*m + m*y/(delta^2) ) )
+    }
+    
+    if (type == "ET")
+    {
+      h <- - m*exp(nu) 
+    }
+  
+  if (type == "HD")
+  {
+    h <-  4*m/( (2 - nu)^2 )
+  }
+  if (type == "CU")
+  {
+    h <- -(1 + nu)*m
+  }
+  
+  grad <- -apply(h,2,sum)/n
+  return(grad)
+}
+
+
+outer <- function(theta,momentfun,dat,type = c("ET","EL","HD","CU","ETEL","ETHD") , smooth = c(TRUE,FALSE) , delta)
+{ 
+  if(missing(delta)) {delta<- 0.001}
+  if(missing(smooth))
+  { smooth = FALSE }
+  
+m <- momentfun( theta, dat )
+  
+  if (smooth == TRUE )
+  {
+    m <- smoothG( m , bw = bwAndrews, prewhite = 0, ar.method = "ols", 
+                        weights = weightsAndrews, kernel = c( "Truncated"),  
+                        tol = 1e-07)$smoothx
+  }
+
+  nmoments <- ncol(m) 
+  n <- nrow(m)
+  # The problem is easy to solve in lambda, so we use a basic algorithm
+ 
+  if (type == "ETEL")
+  {
+    innersol <- optim(matrix(0,nmoments,1), inner , gr = innergradient , theta = theta ,method = "BFGS" ,delta = delta , momentfun = momentfun , type = "ET" , dat = dat , smooth = smooth )
+    m <- momentfun(theta,x = dat )
+    nu <- as.vector(m%*%innersol$par )
+
+    w <- -exp(nu)
+    w <- w/sum(w)
+    w <- ifelse(is.nan(w)==TRUE,1/(n^2),w)
+    # if ( any(w==0)) 
+    #   {
+    #   w <- 1/n
+    # }
+    obj <- - mean(log(n*w))
+    
+  }
+  else if (type == "ETHD")
+  {
+    innersol <- optim(matrix(0,nmoments,1), inner , gr = innergradient , theta = theta ,method = "BFGS" ,delta = delta , momentfun = momentfun , type = "ET" , dat = dat , smooth = smooth )
+    m <- momentfun(theta,x = dat )
+    nu <- as.vector(m%*%innersol$par )
+    w <- -exp(nu)
+
+    w <- w/sum(w)
+    w <- ifelse(is.nan(w)==TRUE,1/(n^2),w)
+    obj <- -4*(sqrt(n*w) - 1 )   # HD likelihood function
+    
+    obj <- mean(obj)
+  }
+  else
+   {
+      innersol <- optim(matrix(0,nmoments,1), inner , gr = innergradient , theta = theta ,method = "BFGS" ,delta = delta , momentfun = momentfun , type = type , dat = dat , smooth = smooth )
+      obj <- -innersol$val 
+     }   # The inner problem returns the solutions to the first minimization 
+
+  return(obj)
+  }
+
+getlambdas <- function(theta,momentfun,dat,type = c("ET","EL","HD","CU"),smooth = c(TRUE,FALSE), delta)
+{    
+  
+  if(missing(smooth))
+  { smooth = FALSE }
+  
+  m <- momentfun( theta, dat )
+  
+  if (smooth == TRUE )
+  {
+    m <- smoothG( m , bw = bwAndrews, prewhite = 1, ar.method = "ols", 
+                  weights = weightsAndrews, kernel = c( "Truncated"), 
+                  tol = 1e-07)$smoothx
+  }
+  
+  nmoments <- ncol(m) 
+  n <- nrow(m)
+  
+  # The problem is easy to solve in lambda, so we use a basic algorithm
+  innersol <- optim(matrix(0,nmoments,1), inner , gr = innergradient, theta = theta ,method = "BFGS" ,delta = delta , momentfun = momentfun , type = type , dat = dat , smooth = smooth )
+  return(innersol$par )   # The inner problem returns the solutions to the first minimization 
+}
+
+impliedprobs <- function(theta, lambda, momentfun, dat, type = c("ET","EL","HD","CU"), smooth = c(TRUE,FALSE))
+{ 
+  
+  if(missing(smooth))
+  { smooth = FALSE }
+  
+  m <- momentfun( theta, dat )
+  
+  if (smooth == TRUE )
+  {
+    m <- smoothG( m , bw = bwAndrews, prewhite = 1, ar.method = "ols", 
+                  weights = weightsAndrews, kernel = c( "Truncated"),  
+                  tol = 1e-07)$smoothx
+  }
+  
+  nmoments <- ncol(m) 
+  n <- nrow(m)
+  
+  nu <- m%*%lambda
+  if (type == "EL")
+  {
+  rho1 <- 1/(1-nu)
+  pi <- rho1/sum(rho1)
+  }
+  if (type == "ET")
+  {
+  pi <- exp(nu)/(sum(exp(as.vector(nu))))
+  }
+  if (type == "HD")
+  {
+    rho1 <- 1/(1-nu)^2
+    pi <-  rho1/sum(rho1)
+  }
+  if (type == "CU")
+  {
+    rho1 <- -(1 + nu)
+    pi <- rho1/sum(rho1) 
+  }
+  return(pi)
+}
+
+likelihood <- function(theta, momentfun, dat, type=c("EL","ET","ETEL","HD","CU","ETHD"), smooth = c(TRUE,FALSE), delta)
+{
+
+  
+  if (type == "EL")
+  {
+    obj <- mean(log(impliedprobs(theta,getlambdas(theta,momentfun=momentfun,dat=dat,type = "EL",smooth = smooth , delta),momentfun=momentfun,dat=dat,type = type , smooth = smooth ))*nrow(dat))
+  }
+  if (type == "ET")
+  {
+    obj <- - mean( nrow(dat)*impliedprobs(theta = theta, lambda = getlambdas(theta, momentfun = momentfun, dat = dat, type = "ET",smooth = smooth,delta),momentfun=momentfun,dat=dat,type = type , smooth = smooth)*log(nrow(dat)*impliedprobs(theta,getlambdas(theta,momentfun=momentfun,dat=dat,type = "ET",smooth = smooth,delta),momentfun=momentfun,dat=dat,type = type , smooth = smooth)) )
+  }
+  if (type == "HD")   # it corresponds to the likelihood of the HD criterion function, but the ET weights are used 
+  {
+    obj <- 4*mean(sqrt( (nrow(dat)* (impliedprobs(theta,getlambdas(theta,momentfun=momentfun,dat=dat,type = "HD",smooth = smooth,delta),momentfun=momentfun,dat=dat,type = "HD",smooth = smooth)) )) - 1 )
+  }
+  if (type == "CU")
+  {
+    obj <- - mean((impliedprobs(theta,getlambdas(theta,momentfun=momentfun,dat=dat,type = "CU",smooth = smooth , delta),momentfun=momentfun,dat=dat,type = type , smooth = smooth )*nrow(dat))^2)
+  }
+  if (type == "ETEL")   # it corresponds to the likelihood of the EL criterion function, but the ET weights are used 
+  {
+    obj <- mean(log(impliedprobs(theta,getlambdas(theta,momentfun=momentfun,dat=dat,type = "ET",smooth = smooth,delta),momentfun=momentfun,dat=dat,type = "ET",smooth = smooth))*nrow(dat))
+  }
+  if (type == "ETHD")   # it corresponds to the likelihood of the HD criterion function, but the ET weights are used 
+  {
+    obj <- 4*mean(sqrt((nrow(dat)* (impliedprobs(theta,getlambdas(theta,momentfun=momentfun,dat=dat,type = "ET",smooth = smooth,delta),momentfun=momentfun,dat=dat,type = "ET",smooth = smooth)) )) - 1 )
+  }
+  
+  return(-obj)
+}
+
+
+GEL <- function(x0 , momentfun, dat, type = c("EL" , "ET" , "ETEL" , "HD" , "CU" , "ETHD") , smooth = c( TRUE , FALSE ) , lb  , ub , delta , trace , cluster , maxiter )
+{
+  if(missing(trace)) {trace <- FALSE }
+  if(missing(cluster)){cluster <- NULL}
+  if(missing(maxiter)){maxiter <- 500}
+  if(missing(delta)){delta <- 0.01}
+  options(warn=-1)
+  
+  n <- nrow(dat)
+  nmoments <- ncol(momentfun( x0, dat ) ) 
+
+outersol <- DEoptim(outer , lower = lb , upper = ub , momentfun = momentfun , dat = dat , delta = delta , type = type ,smooth = smooth, control = DEoptim.control(itermax = maxiter , trace = trace , strategy = 2 ,CR = 1 , NP = 20 , cluster = cluster))
+thetahat <- outersol$optim$bestmem
+Qhat <- outersol$optim$bestval
+
+if (type == "ETEL" | type == "ETHD")
+{
+  lambdahat <- getlambdas(theta=thetahat,momentfun=momentfun,dat=dat,type="ET", smooth = smooth , delta = delta)
+  pihat <- impliedprobs( thetahat , lambdahat , momentfun , dat = dat , type = "ET" , smooth = smooth )
+}
+
+else {
+lambdahat <- getlambdas(theta=thetahat,momentfun=momentfun,dat=dat,type=type,delta = delta)
+pihat <- impliedprobs( thetahat , lambdahat , momentfun , dat = dat , type = type )
+}
+
+return(list( estimates = thetahat , objective = Qhat , multipliers = lambdahat , probabilities = pihat ) )
+}
+
+
---
+++ GMM.R
@@ -0,0 +1,298 @@
+# This function estimates the parameters from moment conditions using the 2-step method or cue method  
+
+# Details ----------------------------
+# This script contains the following functions:
+#   - eq: functions that checks if two matrices x and y have the same elements everywhere
+#   - gbar: for some value of the parameters "theta" and a moment function "momentfun", 
+#           computes the average of each function in "momentfun" and returns a column vector of the size of the number of functions in "momentfun".
+#   - Q: for some value theta, the moment function "momentfun", some data and the weighting matrix W, computes the GMM objective function.
+#   - Qcue: for some value theta, the moment function "momentfun", and some data , computes the continuously updted GMM (CU-GMM) objective function. 
+#           If it is a time series environment, the long run variance of the moments  can be used for the weighting matrix.
+#   - jacobar: compute the average of the jacobian matrix of "momentfun". No numerical derivatives, "jacobian" must be provided.
+#   - secondmom: computes the second moments of a matrix
+#   - GMM: minimizes "Q" (if the method is "twostep", "onestep" or "iterative") or "Qcue" (if the method is "cue") using the "DEoptim" package from Ardia et al. (see https://cran.r-project.org/web/packages/DEoptim/DEoptim.pdf).
+#          Returns the estimates of, the value of the objective function, the estimate of the variance-covariance matrix of the estimates, Hansen's J-test and whether a generalized inverse had to be used for the weighting matrix.  
+
+  
+list.of.packages <- c('DEoptim', 'sandwich', 'MASS','pracma')
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+library('DEoptim')   # algorithm used for the GMM estimation
+library('sandwich')
+library('MASS')
+library('pracma')
+source('lvar.R')
+
+
+
+eq <- function(x,y,tol)   # this function works the best I believe, when the matrices contain numbers. Does not work for logical things or characters yet
+{ 
+  if(missing(tol)){tol <- 0.00001}
+  x <- as.matrix(x)
+  y <- as.matrix(y)
+if( (length(x)!=length(y))==TRUE ) {stop("Dimensions don't match mate !")}
+  iter <- 1
+  while(any(abs(x[iter] - y[iter]) <= tol) && iter <= nrow(x)*ncol(x) )
+  {
+    iter <- iter +1          
+  }
+  if(iter < nrow(x)*ncol(x)) {out <- 'na'}
+  else{out <- 'yes'}
+  return(out)
+}
+
+
+
+gbar <- function (theta,momentfun,dat)    # returns the average value of ghat : gbar
+{
+  g <- apply( momentfun( theta = theta , x = dat ) , 2 , mean )     # mean of each column who represents a different moment
+  
+  return( data.matrix(g) )   # reports a column vector, with as many rows as moments
+}
+
+
+Q <- function (theta,momentfun,dat,W)    # Objective function
+{
+  Q <- crossprod( gbar( theta = theta , momentfun = momentfun , dat = dat ) , W )%*%( gbar( theta = theta , momentfun = momentfun , dat = dat) )
+  return(Q)
+}
+
+Qcue <- function (theta, momentfun, dat, env = c("iid","ts"))    # Objective function
+  
+{
+  if(missing(env)) {env <- "ts"}
+  
+  f1 <- momentfun(theta = theta , dat  )
+  
+  if(env=="iid"){    Wmat <-  cov( f1 )   + gbar( theta = theta , momentfun = momentfun , dat = dat )%*%t( gbar( theta = theta , momentfun = momentfun , dat = dat ) )       # we use a generalized inverse since there are singularity issues. The function cov comes the stats package
+  
+  }   else      {    Wmat <-  longrunvar(f1,center=TRUE,method='HACC_B')[[1]] }
+ 
+  Wcue <- MASS::ginv( Wmat)
+
+  Qcue <- t(gbar(theta,momentfun,dat)) %*% Wcue %*% (gbar(theta,momentfun,dat))
+  
+  return(Qcue)
+}
+
+jacobar <- function (jacobian , theta , dat)    # computes the average jacobian in a not too inefficient way
+{
+  J <- list()
+  for (i in 1:nrow(data.matrix(dat)))
+  {
+    y <- as.matrix( as.matrix(dat)[i,] )
+    J[[i]] <- jacobian( theta , x = t(y))
+  }
+  return((1/nrow(data.matrix(dat)))*Reduce('+',J))
+}
+
+secondmom <- function (momentfun , theta , dat)    # computes the second moments of a matrix
+
+{
+  J <- list()
+  f <- momentfun(theta,dat)
+  
+  for (i in 1:nrow(data.matrix(dat)))
+  {
+    J[[i]] <- cbind(f[i,])%*%rbind(f[i,])
+  }
+  return((1/nrow(data.matrix(dat)))*Reduce('+',J))
+}
+
+GMM <- function( momentfun , x0  , jacobian , dat , method = c("twostep","cue","iterative", "onestep") , env = c("iid","ts") , optimethod = c('optim','DEoptim')  , lb , ub , maxiter , trace , cluster )
+{
+  
+  library(pracma)
+  
+  options(warn=-1)
+  if(missing(env)) {env <- "ts"}
+  if(missing(cluster)) {cluster <- NULL}
+  if(missing(maxiter)) {maxiter <- 500 }    
+  if(missing(trace)) {trace <- FALSE }
+  if(missing(lb) | missing(ub)  ){warnings('Upper or lower bound not supplied for optimethod DEoptim; optim is used instead')
+    optimethod <- "optim"}
+  if(missing(optimethod)){optimethod <- 'optim'}
+
+  n <- nrow(data.matrix(dat) )
+  
+  nmoments <- ncol ( momentfun ( x0  , dat ) )     # number of moments used by evaluating the function at random points. We should find a better way to get that info
+  
+  
+  if (method == "onestep")
+  {
+    if(optimethod=='optim') {
+      step1 <- optim( par = x0 , fn = Q , gr = NULL , dat = dat , momentfun = momentfun ,  W = diag(nmoments) , method = 'Nelder-Mead', control = list(trace=trace) , hessian = FALSE)
+      sol <- step1$par
+      obj <- step1$value
+    }
+    
+    
+    else{step1 <- DEoptim( Q , lower = lb ,upper = ub , control = DEoptim.control(itermax = maxiter ,strategy = 2, trace = trace , cluster = cluster) , momentfun = momentfun , dat = dat , W = diag(nmoments)  )     # Differential evolution algorithm
+    sol <- step1$optim$bestmem    # output when using DEoptim
+    obj <- step1$optim$bestval    }# with DEoptim
+
+  }
+  # Step 1 : minimize gbar'gbar and get W1 with theta1
+  if (method == "twostep")
+  {
+    if(optimethod=='optim') {
+      step1 <- optim( par = x0 , fn = Q , gr = NULL , dat = dat , momentfun = momentfun  , W = diag(nmoments) , method = 'Nelder-Mead', control = list(trace=trace) , hessian = FALSE)
+      theta1 <- step1$par
+      obj1 <- step1$value
+    }
+    
+  else {  
+  step1 <- DEoptim(Q , lower = lb ,upper = ub , dat = dat , momentfun = momentfun , W = diag(nmoments) ,DEoptim.control(itermax = maxiter , trace = trace , strategy = 2, cluster = cluster) )     # Differential evolution algorithm
+   
+  theta1 <- step1$optim$bestmem    # output when using DEoptim
+       }
+    
+  nparam <- nrow(data.matrix(theta1) )    # obtain the dimension of theta here
+  f1 <- momentfun(theta1 , dat )
+  
+  if(env=="iid"){    Wmat <-  var( f1 )   + gbar(theta1 , momentfun ,dat)%*%t( gbar ( theta1 , momentfun , dat ) )       
+
+  }   else      {    Wmat <-  longrunvar(f1,center=TRUE,method='HACC_B')[[1]] }
+  
+  Wmat <- matrix(as.numeric(Wmat),nmoments,nmoments)
+  invindicator<-eq(solve(Wmat),MASS::ginv(Wmat))
+  
+  Wmat <- MASS::ginv(Wmat)
+
+  # Step 2 : minimize gbar' %*% Wmat %*% gbar and get W2 with theta2
+  if(optimethod=='optim') {
+    step2 <- optim( par = x0 , fn = Q , gr = NULL , dat = dat , momentfun = momentfun , W = Wmat , method = 'Nelder-Mead', control = list(trace=trace) , hessian = FALSE)
+    sol <- step2$par
+    obj <- step2$value
+  }
+  else{
+    step2 <- DEoptim(Q , lower = lb ,upper = ub , dat = dat , momentfun = momentfun , W = Wmat ,DEoptim.control(itermax = maxiter ,strategy = 2,trace = trace, cluster = cluster))     # Differential evolution algorithm
+    sol <- step2$optim$bestmem    # when DEoptim is used
+    obj <- step2$optim$bestval
+  }
+  
+  f1 <- momentfun( sol , dat )
+  if(env=="iid"){    Wmat <-  var( f1 )   + gbar(sol , momentfun ,dat)%*%t( gbar ( sol , momentfun , dat ) )       
+  
+  }   else      {    Wmat <-  longrunvar(f1,center=TRUE,method='HACC_B')[[1]] }
+  
+  
+  Wmat <- matrix(as.numeric(Wmat),nmoments,nmoments)
+  invindicator<-eq(solve(Wmat),MASS::ginv(Wmat))
+  
+  Wmat <- MASS::ginv(Wmat)
+  
+  }
+  
+  else if (method == "iterative")
+  {
+    if(optimethod=='optim') {
+      step1 <- optim( par = x0 , fn = Q , gr = NULL , dat = dat , momentfun = momentfun ,  W = diag(nmoments) , method = 'Nelder-Mead', control = list(trace=trace) , hessian = FALSE)
+      theta1 <- step1$par
+      obj1 <- step1$value
+                            }
+    else{
+    step1 <- DEoptim( Q , lower = lb , upper = ub , dat = dat , momentfun = momentfun , W = diag(nmoments) ,DEoptim.control(itermax = maxiter ,strategy = 2,trace = trace, cluster = cluster))     # Differential evolution algorithm
+    
+    theta1 <- step1$optim$bestmem
+    obj1 <- step1$optim$bestval
+        }
+    nparam <- nrow(data.matrix(theta1) )    # obtain the dimension of theta here
+    f1 <- momentfun (theta1 , dat )
+    
+    theta0 <- x0
+    iter <- 0
+    dev <- 1
+    itermax <- 1000
+    
+    while (dev > 0.000001 && iter < itermax)
+    {
+      W1 <- MASS::ginv( cov(( f1 ) ) + gbar(theta1 , momentfun ,dat)%*%t( gbar ( theta1 , momentfun , dat ) )     , tol = sqrt(.Machine$double.eps) )    # we use a generalized inverse since there are singularity issues. The function cov comes the stats package
+      
+      # Step 2 : minimize gbar' %*% W1 %*% gbar and get W2 with theta2
+      if(optimethod=='optim') {
+        step2 <- optim( par = x0 , fn = Q , gr = NULL , dat = dat , momentfun = momentfun ,  W = W1 , method = 'Nelder-Mead', control = list(trace=trace) , hessian = FALSE)
+        thetanew <- step2$par
+        objnew <- step2$value
+                              }
+      else          {
+        step2 <- DEoptim( Q , lower = lb , upper = ub , dat = dat , momentfun = momentfun , W = W1 , DEoptim.control(itermax = maxiter ,strategy = 2,trace = trace, cluster = cluster))     # Differential evolution algorithm
+        thetanew <- step2$optim$bestmem
+        objnew <- step2$optim$bestval
+                    }
+    dev <- abs(obj1 - objnew)
+    fnew <- momentfun (thetanew , dat )      # update if the condition in the while statement is not violated 
+    theta1 <- thetanew
+    obj1 <- objnew
+    f1 <- fnew
+    iter <- iter + 1
+    }
+    
+  sol <- thetanew
+  obj <- objnew
+  Wmat <- W1
+  rm(f1)
+  rm(fnew)
+  rm(dev)
+    
+  }
+  
+  else if (method == "cue")
+  {
+    if(optimethod=='optim') {
+      cuesol <- optim( par = x0 , fn = Qcue , gr = NULL , dat = dat , momentfun = momentfun , env = env , method = 'Nelder-Mead', control = list(trace=trace) , hessian = FALSE)
+      sol <- cuesol$par
+      obj <- cuesol$value
+                            }
+   cuesol <- DEoptim(Qcue , lower = lb ,upper = ub , dat = dat , momentfun = momentfun , env = env  , control = DEoptim.control(itermax = maxiter, strategy = 2 , trace = trace, cluster = cluster))     # Differential evolution algorithm
+  
+    sol <- cuesol$optim$bestmem
+    obj <- cuesol$optim$bestval
+    f1 <- momentfun(sol , dat )
+    
+    if(env=="iid"){    Wmat <-  cov( ( f1 ) ) + gbar(sol , momentfun ,dat)%*%t( gbar ( sol , momentfun , dat ) )       
+    }   else{          Wmat <-  longrunvar(f1,center=TRUE,method='HACC_B')[[1]]  }
+    
+    
+    invindicator<-eq(solve(Wmat),MASS::ginv(Wmat))
+    
+    Wmat <- ginv(Wmat)
+  }
+  
+  f1 <- momentfun(sol , dat )
+  if(env=="iid"){    Wmat <-  cov( ( f1 ) ) #+ gbar(sol , momentfun ,dat)%*%t( gbar ( sol , momentfun , dat ) )       
+  }   else{          Wmat <-  longrunvar(f1,center=TRUE,method='HACC_B')[[1]]  }
+  
+  Wmat <- MASS::ginv(Wmat)
+  
+  Ejac <- jacobar(jacobian , sol , dat)    # Expectd Jacobian, nothing else...
+  VARHAT <- solve(crossprod(Ejac,Wmat)%*%Ejac)/n
+  rm(Ejac)
+  
+  
+  if (method == "iterative")
+  {
+    Jobj <- Q(sol,momentfun,dat,W=Wmat)
+    Jtest <- n*Jobj
+    output <- list(sol,obj,Jtest,VARHAT,iter )
+    names(output) <- c('Estimates','Objective function','Jtest','Estimated_variance','number of iterations')
+    return (output)
+  }
+  else if (method == "onestep")
+  {
+    output <- list( Estimates = sol , Objective = obj )
+    names(output) <- c("Estimates", "Objective function" )
+    
+    return(output)
+  }
+  
+   else 
+  {
+    Jobj <- Q(sol,momentfun,dat,W=Wmat)
+    Jtest <- n*Jobj
+    output <- list(sol, obj, VARHAT, Jtest, invindicator)
+    names(output) <- c("Estimates", "Objective_function", "Estimated_variance", "Jtest", "regular_inverse_used")
+    
+    return (output )
+  }
+}
